7d120368922fbd4102eb41007be3d4582dfc0374
netcam22 <annettelesage@btinternet.com>
2023-11-03 09:28:44 +0000

    feat: implement initial test and function for basic data

--- src/bowling-game.ts
+++ src/bowling-game.ts
@@ -1,3 +1,15 @@
-export function totalScore() {
-  return 5;
+export function totalScore(scoreString: string): number {
+  ///"11 11 11 11 11 11 11 11 11 11"
+  // split
+  const scoreArr = scoreString.split(" ");
+  console.log(scoreArr);
+  // map
+  const mappedScore = scoreArr.map(item => {
+    return [parseInt(item[0]), parseInt(item[1])];
+  });
+  console.log(mappedScore);
+  // reduce
+  return mappedScore.reduce((acc, item) => {
+    return (acc += item[0] + item[1]);
+  }, 0);
 }
--- tests/bowling-game.test.ts
+++ tests/bowling-game.test.ts
@@ -1,1 +1,18 @@
 import { totalScore } from "../src/bowling-game";
+//We will not check for valid rolls or correct number of rolls and frames
+//some sample test data:
+// "11 11 11 11 11 11 11 11 11 11", 20
+//"23 52 45 13 42 33 54 12 34 54",  65
+//"23 52 40 13 42 33 54 12 34 54",  60
+//"9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", 90
+//"5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5", 150
+// "X X X X X X X X X X X X", 300
+describe("test for all ones as throws", () => {
+  // arrange
+  const scoreString = "11 11 11 11 11 11 11 11 11 11";
+  const score = 20;
+  // act and assert
+  test("all throws of one result in score of 20", () => {
+    expect(totalScore(scoreString)).toBe(score);
+  });
+});
