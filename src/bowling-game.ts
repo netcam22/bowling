4d9fe308b38fe123673716c2c4b922d4cd68e15f
netcam22 <annettelesage@btinternet.com>
2023-11-03 10:34:33 +0000

    feat: add further  map parameter values

--- src/bowling-game.ts
+++ src/bowling-game.ts
@@ -4,12 +4,14 @@ export function totalScore(scoreString: string): number {
   const scoreArr = scoreString.split(" ");
   //console.log(scoreArr);
   // map
-  const mappedScore = scoreArr.map((turn: string): Array<number> => {
-    const turnInNums = turn.replace("-", "0");
-    const [a, b] = [parseInt(turnInNums[0]), parseInt(turnInNums[1])];
-    console.log(a, b);
-    return [a, b];
-  });
+  const mappedScore = scoreArr.map(
+    (turn: string, i: number, scoreArray: Array<string>): Array<number> => {
+      const turnInNums = turn.replace("-", "0");
+      const [a, b] = [parseInt(turnInNums[0]), parseInt(turnInNums[1])];
+      console.log(a, b);
+      return [a, b];
+    }
+  );
   //console.log(mappedScore);
   // reduce
   return mappedScore.reduce((acc, item) => {
