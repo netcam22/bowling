67bba7493b431e2e595d9b85afbebe3bb6b2f4f7
netcam22 <annettelesage@btinternet.com>
2023-11-03 22:33:47 +0000

    feat: resolve issue with strikes at end

--- src/bowling-game.ts
+++ src/bowling-game.ts
@@ -1,68 +1,58 @@
 export function totalScore(scoreString: string): number {
-  const scoreArr = `${scoreString} 00 00`.split(" ").reduce(
-    (
-      numArray: Array<Array<number>>,
-      thisTurn: string,
-      i: number,
-      scoreArray: Array<string>
-    ): Array<Array<number>> => {
-      if (i > 1) {
-        const makeSubArr = (scoreStr: string): Array<number> => {
-          return scoreStr
-            .replace("X", "X0")
-            .split("")
-            .map((char: string) =>
-              !isNaN(parseInt(char)) ? parseInt(char) : char === "X" ? 10 : 0
-            );
-        };
-        let [a, b] = makeSubArr(scoreArray[i - 2]);
-        const [c, d] = makeSubArr(scoreArray[i - 1]);
-        let [e, f, ...bonus] = makeSubArr(thisTurn);
-        if (
-          scoreArray[i - 2].includes("/") ||
-          scoreArray[i - 2].includes("X")
-        ) {
-          if (scoreArray[i - 2].includes("/")) {
-            [a, b] = [a, 10 - a + c];
-          } else if (
-            scoreArray[i - 1].includes("X") &&
+  return `${scoreString} 00 00`
+    .split(" ")
+    .reduce(
+      (
+        numArray: Array<Array<number>>,
+        thisTurn: string,
+        i: number,
+        scoreArray: Array<string>
+      ): Array<Array<number>> => {
+        if (i > 1) {
+          const makeSubArr = (scoreStr: string): Array<number> => {
+            return scoreStr
+              .replace("X", "X0")
+              .split("")
+              .map((char: string) =>
+                !isNaN(parseInt(char)) ? parseInt(char) : char === "X" ? 10 : 0
+              );
+          };
+          let [a, b] = makeSubArr(scoreArray[i - 2]);
+          const [c, d] = makeSubArr(scoreArray[i - 1]);
+          let [e, f, ...bonus] = makeSubArr(thisTurn);
+          if (
+            scoreArray[i - 2].includes("/") ||
             scoreArray[i - 2].includes("X")
           ) {
-            [a, b] = [a + b + c + d + e, 0];
-          } else if (scoreArray[i - 2].includes("X")) {
-            [a, b] = [a + b + c + d, 0];
+            if (scoreArray[i - 2].includes("/")) {
+              [a, b] = [a, 10 - a + c];
+            } else if (
+              scoreArray[i - 1].includes("X") &&
+              scoreArray[i - 2].includes("X")
+            ) {
+              [a, b] = [a + b + c + d + e, 0];
+            } else if (scoreArray[i - 2].includes("X")) {
+              [a, b] = [a + b + c + d, 0];
+            }
           }
+          const newArr = [...numArray];
+          if (
+            (i === scoreArray.length - 3 && thisTurn.includes("/")) ||
+            thisTurn.includes("X")
+          ) {
+            let [x, y] = [...bonus];
+            newArr[i - 2] = [a + (x | 0) + (y | 0), b];
+          } else {
+            newArr[i - 2] = [a, b];
+          }
+          return newArr;
         }
-
-        const newArr = [...numArray];
-        if (
-          (i === scoreArray.length - 3 && thisTurn.includes("/")) ||
-          thisTurn.includes("X")
-        ) {
-          let [x, y] = [...bonus];
-          if (x === undefined) {
-            x = 0;
-          }
-          if (y === undefined) {
-            y = 0;
-          }
-          console.log(x, y);
-          newArr[i - 2] = [a + x + y, b];
-        } else {
-          newArr[i - 2] = [a, b];
-        }
-        return newArr;
-      }
-      return numArray;
-    },
-    [
-      [0, 0],
-      [0, 0]
-    ]
-  );
-  //console.log(scoreString, scoreArr);
-  return scoreArr.reduce((acc, turn) => {
-    //console.log(turn[0], turn[1], acc);
-    return (acc += turn[0] + turn[1]);
-  }, 0);
+        return numArray;
+      },
+      [
+        [0, 0],
+        [0, 0]
+      ]
+    )
+    .reduce((acc, turn) => (acc += turn[0] + turn[1]), 0);
 }
