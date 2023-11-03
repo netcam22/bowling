9ac50c9b898ac82db2c81c7816e5ff3e9642680d
netcam22 <annettelesage@btinternet.com>
2023-11-03 07:46:45 +0000

    feat: organise files to start

--- tests/bowling-game.test.ts
+++ tests/bowling-game.test.ts
@@ -1,10 +1,1 @@
 import { totalScore } from "../src/bowling-game";
-
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
-    expect(totalScore()).toBe(15);
-  });
-  it("should return 5 for add(2,3)", () => {
-    expect(totalScore()).toBe(5);
-  });
-});
