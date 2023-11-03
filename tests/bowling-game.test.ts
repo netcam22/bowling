498a1605fca57d4e2d1fb48664213fddc731da89
netcam22 <annettelesage@btinternet.com>
2023-11-03 10:15:19 +0000

    feat: add more tests for positive integer scores

--- tests/bowling-game.test.ts
+++ tests/bowling-game.test.ts
@@ -7,12 +7,18 @@ import { totalScore } from "../src/bowling-game";
 //"9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", 90
 //"5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5", 150
 // "X X X X X X X X X X X X", 300
-describe("test for all ones as throws", () => {
+describe("test for only number values from throws with 10 turns", () => {
   // arrange
   const scoreString = "11 11 11 11 11 11 11 11 11 11";
   const score = 20;
   // act and assert
   test("all throws of one result in score of 20", () => {
     expect(totalScore(scoreString)).toBe(score);
+  });
+  test("throws of all positive integer values result in score of 65", () => {
+    expect(totalScore("23 52 45 13 42 33 54 12 34 54")).toBe(65);
+  });
+  test("throws of all 8s and 1s result in score of 180", () => {
+    expect(totalScore("81 81 81 81 81 81 81 81 81 81")).toBe(90);
   });
 });
